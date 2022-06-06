6c1f58b2935d332be2a75ca246a48a2c2da12acb
Mounir El-Abbas <eamounir1@gmail.com>
2022-06-06 12:26:26 -0400

    moved some redux functions to Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,10 +4,31 @@ import Filters from "../components/Filters";
 import Product from "../components/Product";
 
 const Products = () => {
-  const { productsList, isLoading } = useSelector(store => store.products);
+  const products = useSelector(store => store.products);
 
-  if (isLoading) {
+  let filteredProducts = products.productsList;
+
+  if (products.isLoading) {
     return <div>Loading</div>;
+  }
+
+  if (products.filters.search === "") {
+    filteredProducts = products.productsList;
+  }
+
+  if (products.filters.search !== "") {
+    filteredProducts = filteredProducts.filter(product =>
+      product.title
+        .toUpperCase()
+        .includes(products.filters.search.toUpperCase())
+    );
+  }
+
+  if (products.filters.category !== "all") {
+    let categorySelected = products.filters.category;
+    filteredProducts = filteredProducts.filter(
+      product => product.category === categorySelected
+    );
   }
 
   return (
@@ -16,9 +37,13 @@ const Products = () => {
         <Filters />
       </aside>
       <section className="products-container">
-        {productsList.map(product => (
-          <Product key={product.id} {...product} />
-        ))}
+        {filteredProducts.length < 1 ? (
+          <h3>Sorry, no products matched your search criteria.</h3>
+        ) : (
+          filteredProducts.map(product => (
+            <Product key={product.id} {...product} />
+          ))
+        )}
       </section>
     </div>
   );
